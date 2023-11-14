e40c509424da43b077a335fa9c0bfa5a9bd20852
Reuben Abdul <reu12th@gmail.com>
2023-11-14 07:43:34 +0100

    added file for printing strings

---
+++ prints.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * str_print - prints a string
+ * @val: arg for str
+ * Return: string's length
+ */
+
+int str_print(val_list val)
+{
+	char *s;
+	int i;
+	int len;
+
+	s = va_arg(val, char *);
+	if (s == NULL)
+{
+	s = "(null)";
+	len = _strlen(s);
+
+	i = 0;
+	while (i < len)
+	_putchar(str[i]);
+	i++;
+	return (len);
+
+	else
+{
+	len = _strlen(s);
+	i = 0;
+	while (i < len)
+	_putchar(str[i]);
+	i++;
+	return (len);
+}
+}
+}
