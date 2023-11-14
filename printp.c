c8a6a37bd67f99e9eb716e2b8491b3630ec6f6ae
Daniel Akubada <dasuseyi@gmail.com>
2023-11-14 14:24:33 -0800

    file for handilng pointer printing

---
+++ printp.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * printp - prints a pointer
+ * @val: value.
+ * Return: counter.
+ */
+
+int printp(va_list val)
+{
+	void *p;
+	char *s = "(nil)";
+	long int g;
+	int i, h;
+
+	p = va_arg(val, void *);
+	if (p == NULL)
+	{
+		i = 0;
+		while (s[i] != '\0')
+		{
+			_putchar(s[i]);
+		}
+		return (i);
+	}
+
+	g = (unsigned long int)p;
+	_putchar('0');
+	_putchar('x');
+	h = _printx(g);
+
+	return (g + 2);
+}
