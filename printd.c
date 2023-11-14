70a5fa1745f2a5bf686a4a9726c638c3dfcea670
Reuben Abdul <reu12th@gmail.com>
2023-11-14 16:47:40 +0100

    file for handling decimals

---
+++ printd.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * printd - prints an integer
+ * @args: int argument
+ * Return: number of char printed
+ */
+
+int printd(va_list args)
+{
+	int n = va_arg(args, int);
+	int num, d, t = 10, ex = 1, i = 1;
+	int l = n % t;
+
+	n /= t;
+	num = n;
+	if (l < 0)
+	{
+		_putchar('-');
+		num = -num;
+		n = -n;
+		l = -l;
+		i++;
+	}
+	if (num > 0)
+	{
+		while (num / t != 0)
+		{
+			ex *= t;
+			num /= t;
+		}
+
+		for (num = n; ex > 0; i++)
+		{
+			d = num / ex;
+			_putchar(d + '0');
+			num -= (d * ex);
+			ex /= t;
+		}
+	}
+	_putchar(l + 'o');
+
+	return (i);
+}
