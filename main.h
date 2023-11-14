ac384d85b347a88df65eb58889961022f488a08b
Reuben Abdul <reu12th@gmail.com>
2023-11-14 16:43:39 +0100

    updates on main.h

--- main.h
+++ main.h
@@ -1,9 +1,19 @@
-#define MAIN_H
 #ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
 
 int _putchar(char c);
 int _printf(const char *format, ...);
 int char_print(va_list val);
-int str_print(val_list val);
+int str_print(va_list val);
+int _strlen(char *s);
+int _strlenc(const char *s);
+int print_pc(void);
+int printi(va_list args);
+int printd(va_list args);
+int printb(va_list val);
+int printu(va_list args);
+
 
 #endif
