346ae9c04deda880d2fef04b6294467f71111fde
Daniel Akubada <dasuseyi@gmail.com>
2023-11-14 16:29:17 -0800

    prints.c

--- prints.c
+++ prints.c
@@ -13,25 +13,31 @@ int str_print(va_list val)
 	int len;
 
 	s = va_arg(val, char *);
+
 	if (s == NULL)
-{
-	s = "(null)";
-	len = _strlen(s);
+	{
+		s = "(null)";
+		len = _strlen(s);
 
-	i = 0;
-	while (i < len)
-	_putchar(str[i]);
-	i++;
-	return (len);
+		i = 0;
+		while (i < len)
+		{
+			_putchar(s[i]);
+			i++;
+		}
 
+		return (len);
+	}
 	else
-{
-	len = _strlen(s);
-	i = 0;
-	while (i < len)
-	_putchar(str[i]);
-	i++;
-	return (len);
-}
-}
+	{
+		len = _strlen(s);
+		i = 0;
+		while (i < len)
+		{
+			_putchar(s[i]);
+			i++;
+		}
+
+		return (len);
+	}
 }
